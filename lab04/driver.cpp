2e9c745d0ff7fd5817b813f89b25cd8e5edbd862
Harsimran Singh <Simzftw@gmail.com>
2026-02-05 06:48:55 +0000

    finished

--- lab04/driver.cpp
+++ lab04/driver.cpp
@@ -1,1 +1,36 @@
 // TO DO: Implement the driver main() function to test your code.
+#include <iostream>
+#include "LinkedList.hpp"
+
+using namespace std;
+
+
+int main() {
+    cout << "Hello" << endl;
+    LinkedList<int> list;
+    list.append(100);
+    list.append(230);
+
+    cout << "Element at position 0: " << list.getElement(0) << endl;
+    cout << "Element at position 1: " << list.getElement(1) << endl;
+
+
+    cout << "Testing replace function." << endl;
+    list.replace(0, 500);
+    cout << "Element at position 0 after replace: " << list.getElement(0) << endl;
+
+
+    cout << "Testing Clear and getters." << endl;
+    cout << "position 1 before clear: " << list.getElement(1) << endl;
+    list.clear();
+    cout << "Length after clear: " << list.getLength() << endl;
+    list.append(3000);
+    list.append(4000);
+
+    cout << "testing ostream << operator " << endl;
+    cout << list;
+
+
+
+    return 0;
+}
